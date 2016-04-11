66e2069447c11f36925a7e998d669d57f475e5b5
Vincent Taverna <vinny@getlua.com>
2016-04-11 12:31:24 -0400

    release polyfill

---
+++ index.js
@@ -0,0 +1,10 @@
+// Patch __spread method, React v15 deprecation
+// https://github.com/facebook/react/pull/6444
+// https://github.com/jsdf/coffee-react/issues/34
+// https://github.com/Microsoft/TypeScript/issues/7270#issuecomment-207138701
+'use strict';
+
+_ = require('underscore');
+React = require('react');
+
+React.__spread = _.extend;
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Discussion Threads
+
+[React Pull Request](https://github.com/facebook/react/pull/6444)
+
+[Coffeescript](https://github.com/jsdf/coffee-react/issues/34)
+
+[Typescript](https://github.com/Microsoft/TypeScript/issues/7270#issuecomment-207138701)
